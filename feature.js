907a15ed6ea72edea44dda468effdd13257a50c8
Jens Arps <mail@jensarps.de>
2013-04-03 10:51:20 +0200

    Add info block incl. version

--- feature.js
+++ feature.js
@@ -1,3 +1,12 @@
+/**
+ * AMD-Feature - A loader plugin for AMD loaders.
+ * 
+ * https://github.com/jensarps/AMD-feature
+ *
+ * @author Jens Arps - http://jensarps.de/
+ * @license MIT or BSD - https://github.com/jensarps/AMD-feature/blob/master/LICENSE
+ * @version 1.1.0
+ */
 define(['implementations'], function (implementations) {
 
   return {
